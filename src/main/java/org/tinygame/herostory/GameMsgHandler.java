62b8e08edd3b54a48f1a389a318d4a403d4fff47
MrRobot <584937529@qq.com>
2019-12-27 15:41:26 +0800

    GameMsHandler

--- src/main/java/org/tinygame/herostory/GameMsgHandler.java
+++ src/main/java/org/tinygame/herostory/GameMsgHandler.java
@@ -1,12 +1,27 @@
 package org.tinygame.herostory;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+
+import java.lang.reflect.Array;
 
 public class GameMsgHandler extends SimpleChannelInboundHandler<Object> {
 
     @Override
     public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
         System.out.println("收到客户端消息" + msg);
+        BinaryWebSocketFrame frame = (BinaryWebSocketFrame) msg;
+        ByteBuf byteBuf = frame.content();
+
+        byte[] byteArray = new byte[byteBuf.readableBytes()];
+        byteBuf.readBytes(byteArray);
+        System.out.println("收到的字节 = ");
+        for (byte b : byteArray) {
+            System.out.println(b);
+            System.out.println(",");
+        }
+        System.out.println();
     }
 }
