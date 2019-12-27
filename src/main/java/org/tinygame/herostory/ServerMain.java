2c825a5c153bfa32897ce9a0454b9f28a220fe1c
MrRobot <584937529@qq.com>
2019-12-27 14:50:07 +0800

    GameMsHandler

--- src/main/java/org/tinygame/herostory/ServerMain.java
+++ src/main/java/org/tinygame/herostory/ServerMain.java
@@ -1,10 +1,13 @@
 package org.tinygame.herostory;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.*;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.sctp.nio.NioSctpServerChannel;
+
 import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
@@ -13,16 +16,18 @@ public class ServerMain {
     public static void main(String[] args) {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
+
         ServerBootstrap b = new ServerBootstrap();
         b.group(bossGroup, workerGroup);
-        b.channel(NioSctpServerChannel.class);
+        b.channel(NioServerSocketChannel.class);
         b.childHandler(new ChannelInitializer<SocketChannel>() {
             @Override
             protected void initChannel(SocketChannel ch) throws Exception {
                 ch.pipeline().addLast(
                         new HttpServerCodec(),
                         new HttpObjectAggregator(65535),
-                        new WebSocketServerProtocolHandler("/websocket")
+                        new WebSocketServerProtocolHandler("/websocket"),
+                        new GameMsgHandler()
                 );
             }
         });
@@ -30,7 +35,7 @@ public class ServerMain {
         try {
             ChannelFuture f = b.bind(12345).sync();
 
-            if (f.isSuccess()){
+            if (f.isSuccess()) {
                 System.out.println("服务器启动成功");
             }
 
